da310dd72302ea1cdabfa61a3910e6ee18c6e1fa
pala9999 <prasanthala@gmail.com>
2021-10-10 17:07:52 -0400

    final

--- zipCode.py
+++ zipCode.py
@@ -1,2 +1,13 @@
-zipCode = "12345"
-print(zipCode)
+# zipCode = "12345"
+# print(zipCode)
+
+zipCode=input("Enter zipCode:\n")
+#print(zipCode.isnumeric())        
+if len(zipCode) != 5: 
+    print("Invalid Zip: "+ zipCode +"\t...expecting 5 digit zip")
+    exit()
+elif (zipCode.isnumeric()) == False:
+    print("Invalid zip: "+zipCode +"\t...expecting numbers only") 
+    exit()
+else:
+    print(zipCode)
